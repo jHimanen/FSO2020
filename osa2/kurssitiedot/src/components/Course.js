1c4c2effa7d5a78cd8a4275eb390dad5112662c8
Joel Himanen <joel.himanen@aalto.fi>
2020-10-16 10:46:44 +0300

    Add submissions to exercises 2.2-2.5

--- osa2/kurssitiedot/src/components/Course.js
+++ osa2/kurssitiedot/src/components/Course.js
@@ -6,40 +6,44 @@ const Header = (props) => {
             <h1>{props.course}</h1>
         </div>
     )
-  }
+}
   
-  const Content = (props) => {
-    return (
-        <ul>
-            {props.parts.map(part =>
-                <li key={part.id}>
-                    {part.name} {part.exercises}
-                </li>
-            )}
-        </ul>
-    )
-  }
-  
-  /*
-  const Total = (props) => {
+const Content = (props) => {
     return (
         <div>
-            <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+            {props.parts.map(part =>
+                <p key={part.id}>
+                    {part.name} {part.exercises}
+                </p>
+            )}
         </div>
     )
-  }
-  */
+}
+  
+const Total = (props) => {
 
-  const Course = (props) => {
+    const arrOfExercises = (arr) => {
+        return arr.map(part => part.exercises);
+    }
+
+    return (
+        <div>
+            <h3>Total of {arrOfExercises(props.parts).reduce( (total, num) => {
+                return total + num;
+                })} exercises
+            </h3>
+        </div>
+    )
+}
+
+const Course = (props) => {
     return (
         <div>
             <Header course={props.course.name} />
             <Content parts={props.course.parts} />
+            <Total parts={props.course.parts} />
         </div>
       )
-  }
+}
 
-  //To be added: <Total parts={props.course.parts} />
-
-
-  export default Course;
+export default Course;
