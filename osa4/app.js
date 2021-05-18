07df50bb805ea21a4ae50d35ddcb5d046e44c6d9
Joel Himanen <joel.himanen@aalto.fi>
2021-05-18 19:47:02 +0300

    Complete ex. 4.18-4.19

--- osa4/app.js
+++ osa4/app.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose')
 const logger = require('./utils/logger')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 require('dotenv').config()
 
 mongoose.connect(config.mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
@@ -15,6 +16,6 @@ app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
-
+app.use('/api/login', loginRouter)
 
 module.exports = app
